57ec9c16ae11da55759a807742f8f30a96ac79a8
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-12 16:15:52 +0000

    Main part done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+function domainnamegenerator() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        return pronoun[i] + adj[a] + noun[b];
+      }
+    }
+  }
+}
+console.log(domainnamegenerator());
