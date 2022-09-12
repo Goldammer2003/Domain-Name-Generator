64d778fdb584e9ddd062d01e84655370ad8bb3e0
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-12 16:29:02 +0000

    Errors fixed

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 /* eslint-disable */
 
-function domainnamegenerator() {
-  //write your code here
+//write your code here
 
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let a = 0; a < adj.length; a++) {
-      for (let b = 0; b < noun.length; b++) {
-        return pronoun[i] + adj[a] + noun[b];
-      }
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let b = 0; b < noun.length; b++) {
+      console.log(pronoun[i] + adj[a] + noun[b]);
     }
   }
 }
-console.log(domainnamegenerator());
