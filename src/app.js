f408aa695de1e6c15d248c574d4398e419fe16aa
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-17 16:32:53 +0000

    Website ending domain added

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let ending = [".com", ".de", "us"];
 for (let i = 0; i < pronoun.length; i++) {
   for (let a = 0; a < adj.length; a++) {
     for (let b = 0; b < noun.length; b++) {
-      console.log(pronoun[i] + adj[a] + noun[b]);
+      for (let b = 0; b < noun.length; b++) {
+        console.log(pronoun[i] + adj[a] + noun[b] + ending[c]);
+      }
     }
   }
 }
