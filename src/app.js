21cdb2d00e377fa6881618c0a57b9476b7c95b4f
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-17 17:04:42 +0000

    Additional change

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ let noun = ["jogger", "racoon"];
 let ending = [".com", ".de", "us"];
 for (let i = 0; i < pronoun.length; i++) {
   for (let a = 0; a < adj.length; a++) {
-    for (let b = 0; c < noun.length; c++) {
+    for (let b = 0; b < noun.length; b++) {
       for (let b = 0; c < noun.length; c++) {
         console.log(pronoun[i] + adj[a] + noun[b] + ending[c]);
       }
